d3e438ae64777d055cb0900cc4ff9dc8a37c290e
ttoth2 <118882072+ttoth2@users.noreply.github.com>
2025-12-12 14:16:00 +0100

    migration táblák

--- Frontend/esemenyter/src/components/Privacy.vue
+++ Frontend/esemenyter/src/components/Privacy.vue
@@ -1,4 +1,6 @@
 <template>
+  <h1>Adatvédelem</h1>
+  
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iusto, sint, eum repellat doloribus quidem, quia doloremque voluptas quas consequuntur asperiores officia reiciendis ullam perferendis maxime sed ab iure earum!</p>
 </template>
 
